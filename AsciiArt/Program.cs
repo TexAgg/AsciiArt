5fb09c515de1fbf60b4ae234eae38d7de3862955
Matt Gaikema <mgaikema1@gmail.com>
2017-01-21 20:35:55 -0600

    Return if no arguments passed

--- AsciiArt/Program.cs
+++ AsciiArt/Program.cs
@@ -8,11 +8,15 @@ namespace AsciiArt
 	{
 		public static void Main(string[] args)
 		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Please enter a filename");
+				return;
+			}
+
 			string filename = args[0];
 			AsciiArt ascii = new AsciiArt(Image.FromFile(filename), 100, 100);
 			string result = ascii.Generate();
-			System.Console.Write(result);
-			// For debugging.
 			StreamWriter fwriter = new StreamWriter(Path.GetFileNameWithoutExtension(filename) + ".txt");
 			fwriter.WriteLine(result);
 		}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AsciiArt
 Following the tutorial [here](https://www.codeproject.com/Articles/20435/Using-C-To-Generate-ASCII-Art-From-An-Image).
+
+## Instructions
+Pass a filename as a command-line argument and it will save a `*.txt` file with the same name in the same directory containing an ASCII version of the image.
